886501ef6627ab5ce76da7475b7a5c8660dd95d3
yezarniko <yezarniko@protonmail.com>
2023-08-02 09:51:27 +0630

    refactor: Update imports and apply strict mode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// React imports
+import React from "react"; // Importing the React library
+import ReactDOM from "react-dom/client"; // Importing the ReactDOM library (client-specific)
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Local component imports
+import App from "./App.jsx"; // Importing the App component
+
+// Styles imports
+import "./styles/main.scss"; // Importing the main.scss stylesheet
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
