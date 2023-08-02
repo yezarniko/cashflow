e6c3536b8e5a3b6f3f93600d8ec0d4dc2222cc6e
yezarniko <yezarniko@protonmail.com>
2023-08-02 10:07:43 +0630

    feat: Add image type declarations
    Added type declarations for different image types (svg, png, jpg) in the image.d.ts file.
    The declarations allow TypeScript to recognize these image files as modules and provide type information for their imports.

---
+++ client/types/image.d.ts
@@ -0,0 +1,16 @@
+declare module "*.svg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpg" {
+  const value: string;
+  export default value;
+}
+
+// Add similar declarations for other image types like JPEG, GIF, etc. if needed.
